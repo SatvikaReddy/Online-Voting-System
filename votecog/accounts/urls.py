7044ef7f336e7047a9e81a74b6fd47501da5f94c
SatvikaReddy <satvika2kl@gmail.com>
2020-09-25 01:22:49 +0530

    candidate-form

--- votecog/accounts/urls.py
+++ votecog/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 from .views import home
+from .views import PersonCreateView
 
 app_name = 'accounts'
 urlpatterns = [
    path('home-page/', views.home, name='home-page'),
-   path('profile/', views.profile, name='profile')
+   path('profile/', views.profile, name='profile'),
+   path('cform/', PersonCreateView.as_view(), name='person_form'),
+    path('success/', views.success, name='success'),
 ]
