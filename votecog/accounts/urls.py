dced84b30b264eeb60be3805e9cc8fbfd01e2009
komali <komalibeeram699@gmail.com>
2020-09-30 21:55:13 +0530

    commit message

--- votecog/accounts/urls.py
+++ votecog/accounts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 from .views import home
-from .views import PersonCreateView
+# from .views import PersonCreateView
 from django.conf.urls import url
 
 app_name = 'accounts'
 urlpatterns = [
    path('home-page/', views.home, name='home-page'),
    path('profile/', views.profile, name='profile'),
-   path('cform/', PersonCreateView.as_view(), name='person_form'),
+   path('cform/', views.cform, name='person_form'),
     path('success/', views.success, name='success'),
     path('dash/', views.dashboard, name='dashboard'),
     url(r'^dash/(?P<pk>[0-9]+)/cstatus/$', views.cstatus, name='cstatus'),
