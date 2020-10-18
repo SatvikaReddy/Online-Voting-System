076b28bfad422b0980f8e8afcf93fa9fa7b2fd74
komali <komalibeeram699@gmail.com>
2020-10-18 17:43:06 +0530

    like unlike blog posts

--- votecog/blog/models.py
+++ votecog/blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    liked = models.ManyToManyField(Account, default=None, blank=True, related_name='liked')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -20,7 +21,33 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    @property
+    def num_likes(self):
+        return self.liked.all().count()
 
     def get_absolute_url(self):
         return reverse('post_list')
+    
+LIKE_CHOICES=(
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Like(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.post)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Account,on_delete=models.CASCADE)
+    content = models.TextField(max_length=250)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}-{}'.format(self.post.title, str(self.user.username))
     
